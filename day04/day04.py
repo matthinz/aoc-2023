99785ad0decc6b790a4f565dfcec4736016e35ec
Matt Hinz <matthinz@gmail.com>
2023-12-04 12:01:10 -0800

    Day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,27 +1,60 @@
+import re
 import sys
+
+
+def parse_card(spec):
+    spec = re.sub(r"\s+", " ", spec).strip()
+    m = re.match(r"Card (\d+): (.+)\|(.+)", spec)
+
+    if m is None:
+        raise Exception(f"Could not parse spec: #{repr(spec)}")
+
+    card_index = int(m.group(1))
+    winners = set(int(value.strip()) for value in m.group(2).split(" ") if value != "")
+    numbers = set(int(value.strip()) for value in m.group(3).split(" ") if value != "")
+    winning_number_count = len(winners & numbers)
+
+    return (card_index, winning_number_count)
 
 
 def part1(lines):
     lines = (line.strip() for line in lines)
     lines = [line for line in lines if line != ""]
 
-    def parse_numbers(input):
-        return set(int(value.strip()) for value in input.split(" ") if value != "")
-
     total_points = 0
 
     for line in lines:
-        parts = line.partition(":")
-        winners, numbers = parts[2].split("|")
-        winners = parse_numbers(winners) & parse_numbers(numbers)
-        point_value = 2 ** (len(winners) - 1) if len(winners) > 0 else 0
+        _, winning_number_count = parse_card(line)
+        point_value = 2 ** (winning_number_count - 1) if winning_number_count > 0 else 0
         total_points += point_value
 
     return total_points
 
 
 def part2(lines):
-    pass
+    lines = (line.strip() for line in lines)
+    lines = [line for line in lines if line != ""]
+
+    cards = [parse_card(line) for line in lines]
+    counts = {}
+
+    def count_for(card_index):
+        if card_index in counts:
+            return counts[card_index]
+
+        _, winning_number_count = cards[card_index - 1]
+        copies = cards[card_index : card_index + winning_number_count]
+
+        count = 1
+
+        for copy_index, _ in copies:
+            count += count_for(copy_index)
+
+        counts[card_index] = count
+
+        return counts[card_index]
+
+    return sum(count_for(card_index) for card_index, _ in cards)
 
 
 if __name__ == "__main__":
--- day04/test.py
+++ day04/test.py
@@ -13,7 +13,7 @@ class TestDay04(unittest.TestCase):
     def test_part2(self):
         input_filename = os.path.join(os.path.dirname(__file__), "test_input.txt")
         with open(input_filename, "r") as input_file:
-            self.assertEqual(part2(input_file.readlines()), 0)
+            self.assertEqual(part2(input_file.readlines()), 30)
 
 
 if __name__ == "__main__":
