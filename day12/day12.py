dc9252af7a9e09e76a6a8dd0af64573bc2604962
Matt Hinz <matthinz@gmail.com>
2023-12-15 14:29:02 -0800

    Day 12 optimization (shaves ~1s of runtime)

--- day12/day12.py
+++ day12/day12.py
@@ -4,27 +4,98 @@ from itertools import combinations
 from typing import Iterator, Optional
 
 
-def parse_input(lines: list[str]) -> Iterator[tuple[str, list[int]]]:
+def parse_input(
+    lines: list[str], unfolding_factor: int = 1
+) -> Iterator[tuple[str, list[int]]]:
     lines = (line.strip() for line in lines)
     lines = (line for line in lines if line != "")
 
     for line in lines:
         conditions, sizes = line.split(" ")
-        yield (conditions, [int(value) for value in sizes.split(",")])
+
+        conditions = "?".join([conditions] * unfolding_factor)
+
+        sizes = [int(value) for value in sizes.split(",")]
+        sizes *= unfolding_factor
+
+        yield (conditions, sizes)
 
 
-@staticmethod
-def find_possible_variants(conditions: str) -> Iterator[str]:
+def optimize_unknown_indices(conditions: str, sizes: list[int]) -> tuple[str, set[int]]:
+    unknown_indices = [i for i, _ in enumerate(conditions) if conditions[i] == "?"]
+
+    def set_index(conditions: str, i: int, value: str) -> str:
+        result = list(conditions)
+        result[i] = value
+        return "".join(result)
+
+    def break_into_runs(conditions: str) -> list[str]:
+        return [r for r in conditions.split(".") if r != ""]
+
+    def is_possible(index: int, state: str) -> bool:
+        working_conditions = set_index(conditions, index, state)
+        runs = break_into_runs(working_conditions)
+
+        # The cases here:
+        # 1. runs[0] is all unknown (e.g. '???')
+        # 2. runs[0] is all damaged (e.g. '###')
+        # 3. runs[0] is a mix (e.g. '?#?#')
+        #
+        any_unknown = "?" in runs[0]
+        any_damaged = "#" in runs[0]
+        all_damaged = not any_unknown
+        if all_damaged:
+            # We know that the corresponding size must match this run's
+            # length exactly
+            if sizes[0] != len(runs[0]):
+                return False
+        elif any_unknown and any_damaged:
+            # This is a mix of damaged and unknown.
+            # This run is possible only if the size required <= the length
+            # of the run
+            if sizes[0] > len(runs[0]):
+                return False
+
+        return True
+
+    i = 0
+    while i < len(unknown_indices):
+        unknown_index = unknown_indices[0]
+        damaged_is_possible = is_possible(unknown_index, "#")
+        undamaged_is_possible = is_possible(unknown_index, ".")
+
+        if damaged_is_possible and not undamaged_is_possible:
+            # This index must be damaged
+            conditions = set_index(conditions, unknown_index, "#")
+            unknown_indices[0:1] = []
+            continue
+        elif undamaged_is_possible and not damaged_is_possible:
+            conditions = set_index(conditions, unknown_index, ".")
+            unknown_indices[0:1] = []
+            continue
+
+        i += 1
+
+    return (conditions, set(unknown_indices))
+
+
+def find_possible_variants(conditions: str, sizes: list[int]) -> Iterator[str]:
     """
     Given a conditions string, returns an Iterator that runs through the
     different ways "?" could be filled in
     """
-    unknown_indices = set(i for i, _ in enumerate(conditions) if conditions[i] == "?")
+
+    conditions, unknown_indices = optimize_unknown_indices(conditions, sizes)
+
+    if len(unknown_indices) == 0:
+        yield conditions
+        return
 
     for damaged_count in range(0, len(unknown_indices) + 1):
         for damaged_indices in combinations(unknown_indices, damaged_count):
             damaged_indices = set(damaged_indices)
             new_conditions = list(conditions)
+
             for i in unknown_indices:
                 new_conditions[i] = "#" if i in damaged_indices else "."
             yield "".join(new_conditions)
@@ -45,7 +116,7 @@ def is_valid(conditions: str, sizes: list[int]) -> int:
 
 def count_solutions(input_conditions: str, sizes: list[int]) -> int:
     result = 0
-    for conditions in find_possible_variants(input_conditions):
+    for conditions in find_possible_variants(input_conditions, sizes):
         if is_valid(conditions, sizes):
             result += 1
     return result
--- day12/test.py
+++ day12/test.py
@@ -1,6 +1,13 @@
 import unittest
 
-from day12 import part1, part2, parse_input, count_solutions, find_possible_variants, is_valid
+from day12 import (
+    part1,
+    part2,
+    parse_input,
+    count_solutions,
+    optimize_unknown_indices,
+    is_valid,
+)
 
 
 class TestDay12(unittest.TestCase):
@@ -16,47 +23,27 @@ class TestDay12(unittest.TestCase):
         lines = [line.strip() for line in TEST_INPUT.split("\n")]
         self.assertEqual(part1(lines), 21)
 
-    def test_find_possible_variants(self):
-        tests = [
-            {
-                "input": "##?#?#",
-                "expected": [
-                    "##.#.#",
-                    "####.#",
-                    "##.###",
-                    "######",
-                ],
-            },
-            {"input": "#####", "expected": ["#####"]},
-            {"input": "?", "expected": ["#", "."]},
-        ]
-
-        for i, test in enumerate(tests):
-            actual = list(sorted(find_possible_variants(test["input"])))
-            expected = list(sorted(test["expected"]))
-            self.assertEqual(expected, actual, f"Test {i+1} (\"{test["input"]}\")")
-
     def test_is_valid(self):
         tests = [
             ("###", [3], True),
             ("###", [2], False),
             ("###", [4], False),
-            ("##.#.#", [2,1,1], True),
-            ("##.#.#", [3,1,1], False),
-            ("##.#.#", [2,2,1], False),
-            ("##.#.#", [2,1,2], False),
+            ("##.#.#", [2, 1, 1], True),
+            ("##.#.#", [3, 1, 1], False),
+            ("##.#.#", [2, 2, 1], False),
+            ("##.#.#", [2, 1, 2], False),
         ]
 
         for conditions, sizes, expected in tests:
             actual = is_valid(conditions, sizes)
             self.assertEqual(expected, actual)
 
-
     def test_count_solutions(self):
         tests = [
             {"input": "?###?#????.?.#????# 10,1,1,1,1", "expected": 2},
             {"input": "?.?????#??#?#???? 1,1,1,1,4", "expected": 13},
             {"input": ".?.#??.??.? 2,1", "expected": 3},
+            {"input": "???.????.?..????? 3,1,1,3", "expected": 28},
         ]
 
         for test in tests:
@@ -75,6 +62,15 @@ class TestDay12(unittest.TestCase):
 
         lines = [line.strip() for line in TEST_INPUT.split("\n")]
         self.assertEqual(part2(lines), None)
+
+    def test_optimize_unknown_indices(self):
+        conditions = "?###?#????.?.#????#"
+        sizes = [10, 1, 1, 1, 1]
+
+        expected = "##########.?.#????#"
+
+        (actual, _) = optimize_unknown_indices(conditions, sizes)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == "__main__":
